bbd3553410dc38b3391086cb7249a3852047ec05
lenarx <lenarx@mail.ru>
2021-04-09 08:19:29 +0300

    Update zsh-plugins.sh

--- zsh-plugins.sh
+++ zsh-plugins.sh
@@ -11,3 +11,5 @@ git clone git@github.com:zsh-users/zsh-autosuggestions.git
 wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/history.zsh
 
 wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/key-bindings.zsh
+
+cd $HOME
