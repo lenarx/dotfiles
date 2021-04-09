ab8a9d36c26ee45d5abc0d8b1292846ca70b3c50
lenarx <lenarx@mail.ru>
2021-04-09 08:15:17 +0300

    Create zsh-plugins.sh

---
+++ zsh-plugins.sh
@@ -0,0 +1,13 @@
+mkdir $HOME/.zsh-plugins
+
+cd $HOME/.zsh-plugins
+
+git clone git@github.com:zdharma/fast-syntax-highlighting.git
+
+wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/completion.zsh
+
+git clone git@github.com:zsh-users/zsh-autosuggestions.git
+
+wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/history.zsh
+
+wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/key-bindings.zsh
