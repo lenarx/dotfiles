710fc9d4f4de76b11d63b5c5cd538296a66f3e23
lenarx <lenarx@mail.ru>
2021-04-09 08:32:16 +0300

    Update zsh-plugins.sh

--- zsh-plugins.sh
+++ zsh-plugins.sh
@@ -1,15 +1,13 @@
 mkdir $HOME/.zsh-plugins
 
-cd $HOME/.zsh-plugins
+ZSH_PLUGINS=$HOME/.zsh-plugins
 
-git clone git@github.com:zdharma/fast-syntax-highlighting.git
+git clone git@github.com:zdharma/fast-syntax-highlighting.git $ZSH_PLUGINS/fast-syntax-highlighting
 
-wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/completion.zsh
+git clone git@github.com:zsh-users/zsh-autosuggestions.git $ZSH_PLUGINS/zsh-autosuggestions
 
-git clone git@github.com:zsh-users/zsh-autosuggestions.git
+wget -O $ZSH_PLUGINS/completion.zsh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/completion.zsh
 
-wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/history.zsh
+wget -O $ZSH_PLUGINS/.zsh-plugins/history.zsh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/history.zsh
 
-wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/key-bindings.zsh
-
-cd $HOME
+wget -O $ZSH_PLUGINS/.zsh-plugins/key-bindings.zsh https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/lib/key-bindings.zsh
